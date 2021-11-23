b9931b04ab5b55aa35857c58fcb42d274acb886c
Valentyn Holovkevych <76655890+holovkevych@users.noreply.github.com>
2021-11-23 19:26:07 +0200

    uploading all files

---
+++ 18_color_square/app.js
@@ -0,0 +1,33 @@
+const board = document.querySelector('#board')
+const colors = ['yellow', 'red', 'blue', 'purple', 'green', 'pink', 'lightblue', 'slategray', 'black', 'white']
+const SQUARES_NUMBER = 500
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+	const square = document.createElement('div')
+	square.classList.add('square')
+
+	square.addEventListener('mouseover', setColor)
+
+	square.addEventListener('mouseleave', removeColor)
+
+	board.append(square)
+}
+
+function setColor(event) {
+	const element = event.target
+	const color = getRandomColor()
+	element.style.backgroundColor = color
+	element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+
+function removeColor(event) {
+	const element = event.target
+	element.style.backgroundColor = '#1d1d1d'
+	element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+	const index = Math.floor(Math.random() * colors.length)
+	return colors[index]
+}
